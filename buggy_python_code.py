3fa822590ab65c6c48dab5518c23dfef0a408e87
Martin Suchanek <xsuchan6@fi.muni.cz>
2022-04-25 18:50:31 +0200

    adds buggy python code with some changes

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -8,17 +8,19 @@ import flask
 
 # Input injection
 def transcode_file(request, filename):
-    command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
+    command = 'ffmpeg -i "{source}" output_file.mpg'.\
+        format(source=filename)
     subprocess.call(command, shell=True)  # a bad idea!
 
 
 # Assert statements
-def foo(request, user):
+def fooIsBad(user):
     assert user.is_admin, 'user does not have access'
     # secure code...
 
 
 # Pickles
+
 class RunBinSh(object):
     def __reduce__(self):
         return (subprocess.Popen, (('/bin/sh',),))
